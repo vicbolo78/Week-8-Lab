365f204d23bc825b0dcf2cfad9b235d66becdc75
Victor Colon <vicbolo78@gmail.com>
2022-05-05 00:12:20 +0000

    changes made from theia lab environment

--- onlinecourse/tests.py
+++ onlinecourse/tests.py
@@ -1,3 +1,17 @@
+import unittest
+
 from django.test import TestCase
 
-# Create your tests here.
+class TestQuestion(unittest.TestCasel):
+    def test_is_get_score(self, selected_ids):
+        self.assertNotEqual(all_answers = self.choice_set.filter(is_correct=True).count()
+        self.assertEqual(selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+        
+
+       
+if __name__=='__main__':
+    unittest.main()
